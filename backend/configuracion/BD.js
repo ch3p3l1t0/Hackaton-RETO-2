56bbadb49cf424d5779fdf194c57a3d7cce2593d
Juan Esteban Perea Olivares <jeperea45@gmail.com>
2024-11-25 12:28:56 -0500

    haciendo backend

--- backend/configuracion/BD.js
+++ backend/configuracion/BD.js
@@ -1,5 +1,8 @@
-const {pool} = require('pg');
+const {Pool} = require('pg');
 
 const pool = new Pool{
-    
+    user : 'postgres',
+    host : 'localhost',
+    database: 'Reto-Nido',
+    password: 'admin',
 }
