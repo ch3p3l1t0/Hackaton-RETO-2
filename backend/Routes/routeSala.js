1793a670459b7f55a173dbde11edca9261492b46
ch3p3l1t0 <chepelito2016@gmail.com>
2024-11-26 09:00:06 -0500

    Pruebas de BD

--- backend/Routes/routeSala.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const salasController = require('../controllers/controllerSala');
-
-router.get('/salas', salasController.listSalas);
-router.post('/salas', salasController.createSala);
-router.put('/salas/:idSalas', salasController.updateSala);
-
-module.exports = router;
--- backend/Routes/usuarioRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const usuarioController = require('../controllers/controllerUser');
-
-router.get('/usuarios', usuarioController.listUsuarios);
-router.post('/usuarios', usuarioController.createUsuario);
-router.put('/usuarios/:idUsuario', usuarioController.updateUsuario);
-
-module.exports = router;
