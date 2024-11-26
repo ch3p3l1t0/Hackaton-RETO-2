e47f02710cce4f82b004835fedc8fe25b34e2244
santid21 <sduque04@gmail.com>
2024-11-26 08:16:03 -0500

    Login,Register y footer listo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,16 +5,17 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Error404 from "./pages/Error404";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 import AdminDashboard from "./pages/AdminDashboard";
 import UserDashboard from "./pages/UserDashboard";
 
 function App() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen ">
       <Header />
-      <div className="flex h-[calc(100vh-100px)] items-center justify-center">
+
+      <div className="flex-grow">
         <Routes>
           <Route
             path="/"
@@ -32,7 +33,6 @@ function App() {
             path="/register"
             element={<Register />}
           />
-          {/* Rutas Privadas  */}
           <Route
             path="/dashboard/admin"
             element={<AdminDashboard />}
@@ -48,7 +48,7 @@ function App() {
         </Routes>
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
 
