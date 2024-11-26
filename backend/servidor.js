6a334edf39faebe458101ba64cfe7b6f19fac788
Juan Esteban Perea Olivares <jeperea45@gmail.com>
2024-11-26 09:06:27 -0500

    Full Backend

--- backend/servidor.js
+++ backend/servidor.js
@@ -21,29 +21,6 @@ app.use(cors());  // Para habilitar CORS en tu API
 // Ruta para verificar la conexión a la base de datos
 app.get('/conect', connectDBPG);
 
-
-app.post('/api/reservaciones', async (req, res) => {
-    const { userId, roomId, fecha_inicio, fecha_fin, motivo } = req.body;
-    try {
-      const room = await Room.findByPk(roomId);
-      if (!room) {
-        return res.status(404).json({ message: 'Salón no encontrado' });
-      }
-  
-      const reservation = await Reservation.create({
-        user_id: userId,
-        room_id: roomId,
-        fecha_inicio,
-        fecha_fin,
-        motivo,
-      });
-  
-      res.status(201).json(reservation);
-    } catch (err) {
-      res.status(500).send("Error creando la reserva");
-    }
-  });
-
 app.use('/api', usuarioRoutes);
 app.use('/api', reservasRoutes);
 app.use('/api', salasRoutes);
